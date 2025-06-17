dc9313c73c1b40deddb516cf55c3fe1bd6353a11
Anchal627 <140655663+Anchal627@users.noreply.github.com>
2025-06-18 00:28:02 +0530

    Update Homepage.jsx

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -37,7 +37,7 @@ function Homepage() {
   const HomepageValid = async () => {
     let token = localStorage.getItem("usersdatatoken");
     // console.log(token);
-    const res = await fetch("http://localhost:8000/validuser", {
+    const res = await fetch("https://codeeditor-tfe8.onrender.com/validuser", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
