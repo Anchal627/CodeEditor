ee1bbf2b14f6a67b238d13507d925c09bcf6f798
Anchal627 <140655663+Anchal627@users.noreply.github.com>
2025-06-18 01:10:21 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://codeeditor-tfe8.onrender.com", 
+    origin: "", 
     methods: ["GET", "POST"],
     credentials: true,
   })
