d42fabb141c0bc68255104b87b40650b3014b47b
Anchal Jain <jainanchal000@gmail.com>
2025-06-18 00:20:04 +0530

    fix backend mongo code

--- Backend/db/conn.js
+++ Backend/db/conn.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const DB = process.env.MONGO_URI;
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(DB)
   .then(() => console.log("Database Connected"))
   .catch((err) => {
     console.log(err);
