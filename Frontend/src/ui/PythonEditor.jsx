396bd2479d21c40134f9a6ae30dbdaae2b62e6db
Anchal627 <140655663+Anchal627@users.noreply.github.com>
2025-06-18 00:33:28 +0530

    Update PythonEditor.jsx

--- Frontend/src/ui/PythonEditor.jsx
+++ Frontend/src/ui/PythonEditor.jsx
@@ -162,7 +162,7 @@ function PythonEditor() {
     const input = document.getElementById("input").value;
     // Post request to compile endpoint
     axios
-      .post(`http://localhost:8000/compile`, {
+      .post(`https://codeeditor-tfe8.onrender.com/compile`, {
         code: code,
         language: language,
         input: input,
