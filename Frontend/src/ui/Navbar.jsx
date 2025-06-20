4f221d1e17a8519798bd2cc96ecbe9155719f220
Anchal627 <140655663+Anchal627@users.noreply.github.com>
2025-06-20 22:35:14 +0530

    Update Navbar.jsx

--- Frontend/src/ui/Navbar.jsx
+++ Frontend/src/ui/Navbar.jsx
@@ -94,7 +94,7 @@ function Navbar() {
 
   const handleLogout = async () => {
     let token = localStorage.getItem("usersdatatoken");
-    const res = await fetch("https://codeeditor-tfe8.onrender.com/logout", {
+    const res = await fetch("https://codeeditor-wf2n.onrender.com/logout", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
