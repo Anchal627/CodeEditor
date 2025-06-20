e6e6f710d4e034b4ace8fb3bb3e53bbdb3727d6f
Anchal627 <140655663+Anchal627@users.noreply.github.com>
2025-06-20 22:35:30 +0530

    Update PhpEditor.jsx

--- Frontend/src/ui/PhpEditor.jsx
+++ Frontend/src/ui/PhpEditor.jsx
@@ -164,7 +164,7 @@ function PhpEditor() {
     const input = document.getElementById("input").value;
     // Post request to compile endpoint
     axios
-      .post(`https://codeeditor-tfe8.onrender.com/compile`, {
+      .post(`https://codeeditor-wf2n.onrender.com/compile`, {
         code: code,
         language: language,
         input: input,
